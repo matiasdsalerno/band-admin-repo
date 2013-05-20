dc369c710aa39747c377f9f4a0d2deb724799654
Matias Salerno <matias.d.salerno@gmail.com>
2013-05-20 16:38:32 -0300

    email

--- band/grails-app/domain/ar/com/asambleacristiana/Reminder.groovy
+++ band/grails-app/domain/ar/com/asambleacristiana/Reminder.groovy
@@ -1,5 +1,7 @@
 package ar.com.asambleacristiana
 
+import org.apache.commons.mail.SimpleEmail;
+
 class Reminder {
 
 	String triggerName = "trigger" + id
@@ -8,4 +10,22 @@ class Reminder {
 	
     static constraints = {
     }
+	
+	def sendEmail(grailsApplication) {
+		SimpleEmail email = new SimpleEmail();
+		email.with {
+			hostName = grailsApplication.config.mail.hostName
+			fromAddress = grailsApplication.config.mail.fromAddress
+			subject = "Recordatorio: ${event.name} - ${event.date}"
+			smtpPort = grailsApplication.config.mail.smtpPort
+			setAuthentication grailsApplication.config.mail.username, grailsApplication.config.mail.password
+			message = getMailMessage();
+			
+		}
+	}
+	
+	String getMailMessage() {
+		
+	}
+
 }
--- band/grails-app/jobs/ar/com/asambleacristiana/ScheduleEmailJob.groovy
+++ band/grails-app/jobs/ar/com/asambleacristiana/ScheduleEmailJob.groovy
@@ -1,12 +1,18 @@
 package ar.com.asambleacristiana
 
+import ar.com.asambleacristiana.Reminder
+
 
 
 class ScheduleEmailJob {
     static triggers = {
     }
+	
+	def concurrent = false
 
-    def execute(JobExecutionContext context) {
-        Reminder reminder = context.getProperties()["reminder"]
+    def execute(context) {
+        String reminderId = context.mergedJobDataMap.reminderId
+		Reminder reminder = Reminder.get reminderId
+		reminder.sendEmail();
     }
 }
